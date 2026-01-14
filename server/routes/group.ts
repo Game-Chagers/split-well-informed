046abe43f9ec87de5dad3e1e30d9bde2b797f530
Kyle Greenwood <kylehgreenwood@gmail.com>
2026-01-13 20:22:59 -0500

    Implemented adding & deleting expenses from group API routes

--- server/routes/group.ts
+++ server/routes/group.ts
@@ -133,20 +133,28 @@ group.get("/:groupId", async (req: Request, res: Response) => {
 // Add member to group
 group.post("/:groupId/members", async (req: Request, res: Response) => {
   try {
-    const groupId = req.params.groupId;
+    const groupId = parseInt(req.params.groupId);
     const { userId, email } = req.body;
 
-    const user = await findUserByEmailOrID(userId, email);
+    const group = await prisma.group.findUnique({
+      where: { id: groupId },
+    });
+    if (!group) {
+      return res.status(404).json({ error: `Group with ID ${groupId} not found` });
+    }
 
+    const user = await findUserByEmailOrID(userId, email);
     if (!user) {
-      return res.status(400).json({ error: "Must provide either ID or email" });
+      return res
+        .status(400)
+        .json({ error: "User not found, must provide either ID or email" });
     }
 
     const existingMember = await prisma.groupMember.findUnique({
       where: {
         userId_groupId: {
           userId: user.id,
-          groupId: parseInt(groupId),
+          groupId: groupId,
         },
       },
     });
@@ -157,8 +165,8 @@ group.post("/:groupId/members", async (req: Request, res: Response) => {
         .json({ error: "User is already a member of this group" });
     }
 
-    const group = await prisma.group.update({
-      where: { id: parseInt(groupId) },
+    const updatedGroup = await prisma.group.update({
+      where: { id: groupId },
       data: {
         members: {
           create: {
@@ -168,7 +176,7 @@ group.post("/:groupId/members", async (req: Request, res: Response) => {
       },
     });
 
-    res.json(group);
+    res.json(updatedGroup);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error as Error });
@@ -176,55 +184,267 @@ group.post("/:groupId/members", async (req: Request, res: Response) => {
 });
 
 // Remove member from group
-group.delete("/:groupId/members/:userId", async (req: Request, res: Response) => {
-  try {
-    const groupId = parseInt(req.params.groupId);
-    let userId = parseInt(req.params.groupId);
-    const { email } = req.body;
+group.delete(
+  "/:groupId/members/:userId",
+  async (req: Request, res: Response) => {
+    try {
+      const groupId = parseInt(req.params.groupId);
+      let userId = parseInt(req.params.groupId);
+      const { email } = req.body;
 
-    if (isNaN(userId)) {
-      if (!email) {
+      const group = await prisma.group.findUnique({
+        where: { id: groupId },
+      });
+      if (!group) {
         return res
-          .status(400)
-          .json({ error: "Must provide valid ID or email" });
+          .status(404)
+          .json({ error: `Group with ID ${groupId} not found` });
       }
 
-      const user = await prisma.user.findUnique({
-        where: { email: email },
+      if (isNaN(userId)) {
+        if (!email) {
+          return res
+            .status(400)
+            .json({ error: "Must provide valid ID or email" });
+        }
+
+        const user = await prisma.user.findUnique({
+          where: { email: email },
+        });
+
+        if (!user) {
+          res.status(400).json({ error: `User with email ${email} not found` });
+        } else {
+          userId = user.id;
+        }
+      }
+
+      const existingMember = await prisma.groupMember.findUnique({
+        where: {
+          userId_groupId: {
+            userId: userId,
+            groupId: groupId,
+          },
+        },
       });
 
-      if (!user) {
-        res.status(400).json({ error: `User with email ${email} not found` });
-      } else {
-        userId = user.id;
+      if (!existingMember) {
+        return res
+          .status(400)
+          .json({ error: "User is not a member of this group" });
       }
-    }
 
-    const existingMember = await prisma.groupMember.findUnique({
-      where: {
-        userId_groupId: {
-          userId: userId,
-          groupId: groupId,
+      const groupMember = await prisma.groupMember.delete({
+        where: {
+          userId_groupId: {
+            userId: userId,
+            groupId: groupId,
+          },
         },
-      },
-    });
+      });
 
-    if (!existingMember) {
+      res.json(groupMember);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error as Error });
+    }
+  }
+);
+
+// Expense body structure
+// {
+//   description: "Description",
+//   category: "Food",
+//   amount: 70,
+//   payerId: 2,
+//   splitType: "custom",  // or "equal" or "percent"
+//   splits: [
+//     { userId: 1, amount: 60 }, // Percent or specific amount
+//     { userId: 2, amount: 30 },
+//     { userId: 3, amount: 10 },
+//   ]
+// }
+
+// Add expense to group
+group.post("/:groupId/expenses", async (req: Request, res: Response) => {
+  try {
+    const groupId = parseInt(req.params.groupId);
+    const { description, category, amount, payerId, splitType, splits } =
+      req.body;
+
+    if (!description || !amount || !payerId || !splits || splits.length == 0) {
       return res
         .status(400)
-        .json({ error: "User is not a member of this group" });
+        .json({ error: "Missing one or more required fields" });
     }
 
-    const groupMember = await prisma.groupMember.delete({
+    const payerInGroup = await prisma.groupMember.findUnique({
       where: {
         userId_groupId: {
-          userId: userId,
+          userId: payerId,
           groupId: groupId,
         },
       },
     });
+    if (!payerInGroup) {
+      return res
+        .status(400)
+        .json({ error: "Payer not a member of this group" });
+    }
 
-    res.json(groupMember);
+    const usersInSplit = splits.map((split: any) => split.userId);
+    const membersInGroup = await prisma.groupMember.findMany({
+      where: {
+        groupId: groupId,
+        userId: { in: usersInSplit },
+      },
+    });
+    if (usersInSplit.length !== membersInGroup.length) {
+      return res
+        .status(400)
+        .json({ error: "All participants of split must be in the group" });
+    }
+
+    let splitsToAdd;
+    const amountCents = Math.round(amount * 100);
+
+    // Equal split
+    if (splitType == "equal") {
+      const baseSplitCents = Math.floor(amountCents / usersInSplit.length);
+      let remainder = amountCents % usersInSplit.length;
+
+      splitsToAdd = splits.map((split: any, index: number) => {
+        let splitCents = baseSplitCents;
+        if (remainder > 0) {
+          splitCents += 1;
+          remainder -= 1;
+        }
+        return {
+          userId: split.userId,
+          amount: splitCents / 100,
+        };
+      });
+    }
+
+    // Percentage split
+    else if (splitType == "percent") {
+      const totalPercent = splits.reduce(
+        (sum: number, split: any) => sum + split.amount
+      );
+      if (totalPercent != 100) {
+        return res.status(400).json({
+          error: `Percent splits sum ${totalPercent} not equal to 100%`,
+        });
+      }
+
+      const splitsCents = splits.map((split: any) => ({
+        userId: split.userId,
+        cents: Math.floor((amountCents * split.amount) / 100),
+      }));
+      const totalAssigned = splitsCents.reduce(
+        (sum: number, split: any) => sum + split.cents
+      );
+      let remainder = amountCents - totalAssigned;
+
+      splitsToAdd = splitsCents.map((split: any, index: number) => {
+        let finalSplit = split.cents;
+        if (index < remainder) {
+          finalSplit += 1;
+          remainder -= 1;
+        }
+        return {
+          userId: split.userId,
+          amount: finalSplit / 100,
+        };
+      });
+    }
+
+    // Custom amount split
+    else if (splitType == "custom") {
+      const totalAssigned = splits.reduce(
+        (sum: number, split: any) => sum + split.amount
+      );
+      if (totalAssigned != amount) {
+        return res.status(400).json({
+          error: `Cutsom split total ${totalAssigned} not equal to total expense cost ${amount}`,
+        });
+      }
+
+      splitsToAdd = splits.map((split: any) => ({
+        userId: split.userId,
+        amount: split.amount,
+      }));
+    } else {
+      return res.status(400).json({
+        error: `Split type ${splitType} invalid. Valid options: "equal", "percent", "custom"`,
+      });
+    }
+
+    const newExpense = await prisma.expense.create({
+      data: {
+        description: description,
+        category: category,
+        amount: amount,
+        payerId: payerId,
+        groupId: groupId,
+        splits: {
+          create: splitsToAdd,
+        },
+      },
+      include: {
+        payer: true,
+        splits: { include: { user: true } },
+      },
+    });
+
+    res.json(newExpense);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: error as Error });
+  }
+});
+
+// Delete expense
+group.delete(
+  "/:groupId/expenses/:expenseId",
+  async (req: Request, res: Response) => {
+    try {
+      const groupId = parseInt(req.params.groupId);
+      const expenseId = parseInt(req.params.expenseId);
+
+      const expense = await prisma.expense.findUnique({
+        where: { id: expenseId },
+      });
+      if (!expense) {
+        return res
+          .status(404)
+          .json({ error: `Expense with id ${expenseId} not found` });
+      } else if (expense.groupId != groupId) {
+        return res.status(400).json({
+          error: `Expense id ${expenseId} does not belong to group id ${groupId} `,
+        });
+      }
+
+      const deletedExpense = await prisma.expense.delete({
+        where: { id: expenseId },
+      });
+      res.json(deletedExpense);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ error: error as Error });
+    }
+  }
+);
+
+// Get all expenses from group
+group.get("/:groupId/expenses", async (req: Request, res: Response) => {
+  try {
+    const groupId = parseInt(req.params.groupId);
+    const expenses = await prisma.group.findUnique({
+      where: { id: groupId },
+      include: { expenses: true },
+    }).expenses;
+    res.json(expenses);
   } catch (error) {
     console.error(error);
     res.status(500).json({ error: error as Error });
