33a6af982d755c0cd2544eff7b2f133bc2af3c1d
Kyle Greenwood <kylehgreenwood@gmail.com>
2026-01-15 14:09:42 -0500

    Fixed a couple type errors and import paths

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,8 @@
 import cookieParser from 'cookie-parser';
 import cors from 'cors'; // Adds special HTTP headers to tell browser to allow requests from different domains (our react native expo code)
-import express, { Request, Response } from 'express'; // Allows creating paths for API
-import prisma from './db.js';
+import express from 'express'; // Allows creating paths for API
 import group from './routes/group.js';
+import user from './routes/users.js';
 
 const app = express();
 
